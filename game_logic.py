9189010bc7dee34745e8b9dc2e1b00f7f155e686
Eugene <dolmatov261997@gmail.com>
2023-11-02 02:21:20 +0200

    Додано рівень складності

--- game_logic.py
+++ game_logic.py
@@ -1,11 +1,17 @@
 import random
 
-def guess_the_number():
+def guess_the_number(difficulty="easy"):
+    if difficulty == "easy":
+        attempts = 10
+    elif difficulty == "medium":
+        attempts = 5
+    else:
+        attempts = 3
+
     secret_number = random.randint(1, 100)
-    attempts = 5
     used_numbers = set()
 
-    while True:
+    while attempts > 0:
         guess = int(input("Вгадайте число від 1 до 100: "))
         if guess in used_numbers:
             print("Ви вже вводили це число.")
@@ -15,14 +21,16 @@ def guess_the_number():
             print("Спробуйте менше число.")
         else:
             print(f"Вітаємо! Ви вгадали число {secret_number}.")
-            attempts = 0
+            break
+
         used_numbers.add(guess)
         attempts -= 1
         print(f"У вас залишилося {attempts} спроб.")
         print(f"Використані числа: {', '.join(map(str, used_numbers))}")
-        if attempts == 0:
-            print(f"Ви програли. Правильне число було {secret_number}.")
-            break
+
+    if attempts == 0:
+        print(f"Ви програли. Правильне число було {secret_number}.")
 
 if __name__ == "__main__":
     guess_the_number()
+ 
--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import game_logic
 
 layout = [
     [sg.Button("Почати гру")],
+    [sg.Radio("Легкий", "RADIO1", default=True, size=(10, 1), key="-EASY-"), sg.Radio("Середній", "RADIO1", key="-MEDIUM-")],
 ]
 
 window = sg.Window("Графічний інтерфейс", layout)
 game_thread = None  
 
-def start_game():
+def start_game(difficulty):
     global game_thread
     if game_thread is None or not game_thread.is_alive():
-        game_thread = threading.Thread(target=game_logic.guess_the_number)
+        game_thread = threading.Thread(target=game_logic.guess_the_number, args=(difficulty,))
         game_thread.start()
 
 while True:
@@ -21,6 +22,8 @@ while True:
     if event == sg.WINDOW_CLOSED:
         break
     if event == 'Почати гру':
-        start_game()
+        difficulty = "easy" if values["-EASY-"] else "medium"
+        start_game(difficulty)
 
 window.close()
+ 
